8835695ee325c0f173fba7c8ee11a53f5f02b95e
Sasha Kom <xandria97@mail.ru>
2023-02-01 16:29:10 +0300

    измение метода деления с перехватом деления на ноль

--- src/block3/unit1/task1/Calculator.java
+++ src/block3/unit1/task1/Calculator.java
@@ -10,7 +10,15 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> y != 0 ?  x / y : 0 ;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        int result = 0;
+        try {
+            result = x / y;
+        } catch (Exception e) {
+            System.out.println("Ошибка деления");
+        }
+        return result;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- src/block3/unit1/task1/Main.java
+++ src/block3/unit1/task1/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(2, 1);
+        int b = calc.minus.apply(1, 1);
         int c = calc.devide.apply(a, b);
 
         calc.println.accept(c);
