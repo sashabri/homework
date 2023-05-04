7e273afcaa9862eafcceef28ccd5c0d851c55bfd
Sasha Kom <xandria97@mail.ru>
2023-05-04 23:02:15 +0300

    block5 unit1 task1 испр.

--- src/main/java/block5/unit1/task1/Main.java
+++ src/main/java/block5/unit1/task1/Main.java
@@ -10,45 +10,41 @@ public class Main {
         List<Thread> threads = new ArrayList<>();
 
         String[] texts = new String[25];
-
         for (int i = 0; i < texts.length; i++) {
-
-            int finalI = i;
-            Thread newThread = new Thread(
-                    () -> texts[finalI] =  generateText("aab",30000)
-            );
-            newThread.start();
-
-            threads.add(newThread);
+            texts[i] = generateText("aab", 30000);
         }
-
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
 
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
+            Thread newThread = new Thread(
+                    () -> {
+                        int maxSize = 0;
+                        for (int i = 0; i < text.length(); i++) {
+                            for (int j = 0; j < text.length(); j++) {
+                                if (i >= j) {
+                                    continue;
+                                }
+                                boolean bFound = false;
+                                for (int k = i; k < j; k++) {
+                                    if (text.charAt(k) == 'b') {
+                                        bFound = true;
+                                        break;
+                                    }
+                                }
+                                if (!bFound && maxSize < j - i) {
+                                    maxSize = j - i;
+                                }
+                            }
                         }
+                        System.out.println(text.substring(0, 100) + " -> " + maxSize);
                     }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            );
+            newThread.start();
+            threads.add(newThread);
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
         }
         long endTs = System.currentTimeMillis(); // end time
 
