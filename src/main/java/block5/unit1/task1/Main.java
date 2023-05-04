8f83c3c243106aabfa5dc6b40eef4ff8a23865f1
Sasha Kom <xandria97@mail.ru>
2023-05-04 19:04:55 +0300

    block5 unit1 task 1 Интервал значений

---
+++ src/main/java/block5/unit1/task1/Main.java
@@ -0,0 +1,66 @@
+package block5.unit1.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
+
+        String[] texts = new String[25];
+
+        for (int i = 0; i < texts.length; i++) {
+
+            int finalI = i;
+            Thread newThread = new Thread(
+                    () -> texts[finalI] =  generateText("aab",30000)
+            );
+            newThread.start();
+
+            threads.add(newThread);
+        }
+
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+
+        long startTs = System.currentTimeMillis(); // start time
+        for (String text : texts) {
+            int maxSize = 0;
+            for (int i = 0; i < text.length(); i++) {
+                for (int j = 0; j < text.length(); j++) {
+                    if (i >= j) {
+                        continue;
+                    }
+                    boolean bFound = false;
+                    for (int k = i; k < j; k++) {
+                        if (text.charAt(k) == 'b') {
+                            bFound = true;
+                            break;
+                        }
+                    }
+                    if (!bFound && maxSize < j - i) {
+                        maxSize = j - i;
+                    }
+                }
+            }
+            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        }
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
